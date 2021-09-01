06c1a0e4fbbedeff95c1c7f3fc92f0041c6e92fb
Kamal Singh Yadav <kamal.yadav@impetus.com>
2021-09-01 13:36:17 +0530

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "demo-terraform-workflow"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-github-actions"
     }
   }
 }
